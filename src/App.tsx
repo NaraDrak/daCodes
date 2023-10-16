ac4ef8f6160e55a1ee8b1edeb5d8f221e1bf0875
Memo dev <guillermo.villarreal@onephase.com>
2023-10-16 00:49:52 -0600

    app setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import { ThemeProvider } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import MainLayout from './components/MainLayout/MainLayout';
 import theme from './styles/theme';
-import ChipButton from './components/ChipButton/ChipButton';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <MainLayout/>
-      <ChipButton />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <MainLayout/>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
