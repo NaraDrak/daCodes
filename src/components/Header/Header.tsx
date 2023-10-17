9a8c3bfb2029ccdddef0ff6e7ff7830274b66641
Memo dev <guillermo.villarreal@onephase.com>
2023-10-17 00:38:39 -0600

    logout añadido

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,30 +1,38 @@
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import WebStoriesIcon from '@mui/icons-material/WebStories';
 import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
-import styles from './styles.module.css'
 import { useNavigate } from 'react-router-dom';
-interface Props {}
+import styles from './styles.module.css';
+interface Props { }
 
-const Header:React.FC<Props> = () => {
+const Header: React.FC<Props> = () => {
+
   const navigate = useNavigate();
+
   const handleGuestLogout = (): void => {
     localStorage.removeItem('guest_session_id');
     navigate('/login');
   }
 
+  const token = localStorage.getItem('guest_session_id')
+
+  let validSesion: boolean;
+  token !== null ? validSesion = true : validSesion = false 
+
+
   return (
     <AppBar position="static" className={styles.AppBar}>
       <Toolbar className={styles.Toolbar}>
-        
         <Typography variant="h6">
-          <WebStoriesIcon/>
-           DaCodes.
+          <WebStoriesIcon />
+          DaCodes.
         </Typography>
-        
-        <IconButton color="inherit" onClick={handleGuestLogout}>
-          <AccountCircleOutlinedIcon fontSize='large'/>
+        <IconButton  color="inherit" onClick={handleGuestLogout}>
+          <Typography variant='h6' marginRight={1}>
+            {validSesion ? 'Cerra sesión' : ''} 
+          </Typography>
+          <AccountCircleOutlinedIcon fontSize='large' />
         </IconButton>
-
       </Toolbar>
     </AppBar>
   );
